bc01207cfb069c6c6477050f7551637a0b91ff93
PESCHARD François <71873443+Peschard-Francois@users.noreply.github.com>
2020-11-17 18:09:17 +0100

    Add files via upload
    Projet Seral

---
+++ 6-Site Web/Site/main.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('.btn');
+let menu = document.querySelector('.menu');
+let nav = document.querySelector('.nav');
+
+
+btn.addEventListener('click', event => {
+
+
+    menu.classList.toggle('show');
+    nav.classList.toggle('show');
+    
+
+
+
+})
+
+
+
+
